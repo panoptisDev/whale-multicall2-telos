885e775c727b0d16e246ed58eb265b4398880a7f
panoptisDev <panoptis.network@gmail.com>
2022-09-14 22:48:28 +0200

    update deployment script

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 ## whale-multicall2-telos
 🐳 multicallV2 with truffle-config 🐳
 
+## https://sourcify.dev/#/verifier
+drag Build folder and contract folder.
+add contract address
+if fail first time try again.
 Deployed to Testnet
-contract:
+contract: Multicall2@0x50dDe7cD84B2C6f365109a1131Ccd557DbACCaa1
 
 
--- truffle-config.js
+++ truffle-config.js
@@ -1,77 +1,109 @@
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const privateKey = [
-  "" //REPLACE with your deployment private key
-];
- 
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+
 module.exports = {
-  plugins: ["truffle-plugin-verify"],
-  api_keys: {
-    etherscan: "", //Replace with your etherscan key for verification
+
+networks: {
+
+  development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard BSC port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    bscTestnet: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
+    bsc: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    polygon: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://rpc-mainnet.matic.network`),
+      network_id: 137,
+      confirmations: 3,
+      timeoutBlocks: 2000,
+      skipDryRun: true
+    },
+    polygonTestnet: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://rpc.ankr.com/polygon_mumbai`), //if fail change RPC
+      network_id: 80001,
+      confirmations: 4,
+      timeoutBlocks: 9000,
+      skipDryRun: true
+    },
+    telos: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://mainnet.telos.net/evm`),
+      network_id: 40,
+      confirmations: 2,
+      gasPrice: 5100000000000,
+      timeoutBlocks: 2000,
+      skipDryRun: true
+    },
+    telosTestnet: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://testnet.telos.net/evm`),
+      network_id: 41,
+      confirmations: 2,
+      gasPrice: 504374890480,
+      timeoutBlocks: 2000,
+      skipDryRun: true
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://rpc.ankr.com/eth_rinkeby`),
+      network_id: 4,
+      confirmations: 2,
+      timeoutBlocks: 2000,
+      skipDryRun: true
+    }
   },
-  networks: {
-    
-development: {
-      host: "127.0.0.1", // Localhost (default: none)
-      port: 8545, // Standard Ethereum port (default: none)
-      network_id: "*", // Any network (default: none)
-    },
-
-mainnet: {
-      provider: () =>
-        new HDWalletProvider(        "a",
-          `https://smartbch.greyh.at` //Replace it with the network RPC provider
-        ),
-      network_id: 10000, // Update the Chain ID
-      confirmations: 2, // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
-    },
-
-kovan: {
-      provider: () =>
-        new HDWalletProvider("a",
-          `https://kovan.infura.io/v3/55d06c96bc8e4d1b80b4099c6bd1819c` //Replace it with the network RPC provider
-        ),
-      network_id: 42, // Update the Chain ID
-      confirmations: 2, // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
-    },
-
-bsc: {
-      provider: () =>
-        new HDWalletProvider(        "a",
-          `wss://bsc-ws-node.nariox.org:443` //Replace it with the network RPC provider
-        ),
-      network_id: 56, // Update the Chain ID
-      confirmations: 2, // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
-    },
-
-telostestnet: {
-      provider: () =>
-        new HDWalletProvider("",
-          `https://testnet.telos.net/evm` //Replace it with the network RPC provider
-        ),
-      network_id: 41, // Update the Chain ID
-      confirmations: 2, // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
-    },
-
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    etherscan: '',
+    bscscan: process.env.BSCSCAN_API_KEY,
+    polygonscan: process.env.POLYGONSCAN_API_KEY,
+    hecoinfo: '',
+    ftmscan: '',
+  },
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    timeout: 100000
   },
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.16",  
-       optimizer: {
+      //https://forum.openzeppelin.com/t/how-to-deploy-uniswapv2-on-ganache/3885
+      version: "0.8.16",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+      optimizer: {
         enabled: true,
-        runs: 500,
+        runs: 999
       },
-     },
-  },
-  db: {
-    enabled: false,
-  },
-};
+      //evmVersion: "istanbul", 
+      outputSelection: {
+        "*": {
+          "": [
+            "ast"
+          ],
+          "*": [
+            "evm.bytecode.object",
+            "evm.deployedBytecode.object",
+            "abi",
+            "evm.bytecode.sourceMap",
+            "evm.deployedBytecode.sourceMap",
+            "metadata"
+          ]
+        },
+      }
+    },
+  }
+},
+}
